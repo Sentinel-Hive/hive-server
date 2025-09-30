b140899cf404cf58a470c4b62e862fd796b778ab
Austin Adams <asadams110102@gmail.com>
2025-09-30 16:09:46 -0500

    Default values and logging

--- src/svh/commands/server/crud.py
+++ src/svh/commands/server/crud.py
@@ -5,11 +5,24 @@ from pathlib import Path
 from svh import notify
 
 PID_FILE = Path(".svh_server.pid")
+DEFAULT_HOST = "127.0.0.1"
+DEFAULT_PORT = 5167
 
 
 def start_server(config: dict):
-    host = config.get("server", {}).get("host")
-    port = config.get("server", {}).get("port")
+
+    server_cfg = config.get("server", {})
+    host = server_cfg.get("host")
+    port = server_cfg.get("port")
+
+    if not host:
+        notify.error(f"No host specified in configuration: [host: {host}]")
+        notify.server(f"Defaulting to port:{DEFAULT_HOST}")
+        host = DEFAULT_HOST
+    if not port:
+        notify.error(f"No port specified in configuration: [port: {port}]")
+        notify.server(f"Defaulting to port:{DEFAULT_PORT}")
+        port = DEFAULT_PORT
 
     cmd = [
         "uvicorn",
@@ -22,6 +35,7 @@ def start_server(config: dict):
         "--log-level",
         "critical",
     ]
+
     try:
         process = subprocess.Popen(
             cmd,
